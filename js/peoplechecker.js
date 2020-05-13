0ccf0a62a4953b3bc6ed38262d90716caa2c8285
Seongho Jin <seongho4996@naver.com>
2020-05-13 17:30:46 +0900

    feat : peoplecheckerclass

---
+++ js/peoplechecker.js
@@ -0,0 +1,33 @@
+'use strict'
+
+
+
+const ABSENT = 0;
+const ATTEND = 1;
+
+function attendanceChecker(people){
+    let _this = this
+    _this.count = people.length;
+    _this.attendance = {};
+    _this.ABSENT = 0;
+    _this.ATTEND = 1;
+
+    people.forEach(person => {
+        _this.attendance[person.name] = _this.ABSENT;        
+    })
+
+}
+
+attendanceChecker.prototype.attend = function(person){
+    let _this = this;
+    _this.attendance[person.name] = _this.ATTEND;
+}
+
+attendanceChecker.prototype.checkAbsentPeople = function(){
+    let _this = this;
+    let absentPeople = [];
+    for(const name in _this.attendance){
+        if(_this.attendance[name] === _this.ABSENT)absentPeople.push(name);
+    }
+    return absentPeople;
+}
