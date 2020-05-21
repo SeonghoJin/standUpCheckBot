95b2ed7bdfb14bec9e4bb5e0559d175ad3a447a2
Seongho Jin <seongho4996@naver.com>
2020-05-21 17:13:48 +0900

    chore : arrange files

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Schedule = require('node-schedule-tz');
 const fs = require('fs');
 
 const token = fs.readFileSync('./token.txt','utf-8'); 
-const testChannel = 'D013SPP9MFC';
+const testChannel = 'CQPBZNR2S';
 const rtm = new RTMClient(token);
 
 let userschecker = new UsersChecker([]);
@@ -16,45 +16,7 @@ let messageAbsentUsersRule = makeScheduleRule({hour : 1, minute : 30, tz : 'Asia
 //let test_setUsersCheckerRule = makeScheduleRule({minute : 51, tz : 'Asia/Seoul'});
 //let test_messageAbsentUsersRule = makeScheduleRule({minute : 50, tz : 'Asia/Seoul'});
 
-
 setUsersChecker();
-
-/* Test Code
- *(function test_setUsersCheckerJob(test_setUsersCheckerRule, setUsersChecker){
- *    Schedule.scheduleJob(test_setUsersCheckerRule, setUsersChecker);
- *})(test_setUsersCheckerRule, setUsersChecker);
- *
- *(function test_messageAbsentUsersJob(test_messageAbsentUsersRule, messageAbsentUsers){
- *    Schedule.scheduleJob(test_messageAbsentUsersRule, messageAbsentUsers);
- *})(test_messageAbsentUsersRule, messageAbsentUsers);
- */
-
-(function setUsersCheckerJob(setUsersCheckerRule, setUsersChecker){
-    Schedule.scheduleJob(setUsersCheckerRule, setUsersChecker);
-})(setUsersCheckerRule, setUsersChecker);
-
-(function messageAbsentUsersJob(messageAbsentUsersRule, messageAbsentUsers){
-    Schedule.scheduleJob(messageAbsentUsersRule, messageAbsentUsers);
-})(messageAbsentUsersRule, messageAbsentUsers);
-
-
-rtm.on('message', function(event){
-    userschecker.attend(event);
-    console.log(event.username || event.user);
-    if(event.text === 'Hello'){
-        rtm.postMessage(testChannel, "Hi! how are you");
-    }
-    else if(event.text === "Who's absent"){
-        messageAbsentUsers();
-    }
-})
-
-rtm.on('goodbye', function(event){
-    rtm.postMessage(testChannel, "Good Bye");
-})
-
-
-rtm.start();
 
 function messageAbsentUsers(){ // 10:30 시작할 함수 
     rtm.postMessage(testChannel, userschecker.checkAbsentUsers());
@@ -82,3 +44,24 @@ function makeScheduleRule(date){
     }
     return rule;
 }
+
+(function setUsersCheckerJob(setUsersCheckerRule, setUsersChecker){
+    Schedule.scheduleJob(setUsersCheckerRule, setUsersChecker);
+})(setUsersCheckerRule, setUsersChecker);
+
+(function messageAbsentUsersJob(messageAbsentUsersRule, messageAbsentUsers){
+    Schedule.scheduleJob(messageAbsentUsersRule, messageAbsentUsers);
+})(messageAbsentUsersRule, messageAbsentUsers);
+
+rtm.on('message', function(event){
+    userschecker.attend(event);
+    if(event.text === 'Hello'){
+        rtm.postMessage(event.channel, "Hi! how are you");
+    }
+    else if(event.text === "Who's absent"){
+        rtm.postMessage(event.channel, userschecker.checkAbsentUsers());
+    }
+})
+
+rtm.start();
+
