62f92ab0ac0e217a842fdbf29a5766d2e2b00d11
Seongho Jin <seongho4996@naver.com>
2020-05-25 11:49:23 +0900

    test

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-/*'use strict'
+'use strict'
 const RTMClient = require('./js/myRTM');
 const User = require('./js/user');
 const UsersChecker = require('./js/userchecker');
 const messageTemplate = require('./js/messagetemplate');
+const {createEventAdapter} = require('@slack/events-api');
 const Schedule = require('node-schedule-tz');
 const fs = require('fs');
 
 const token = fs.readFileSync('./token.txt','utf-8'); 
-const testChannel = 'D013SPP9MFC';
-const chatChannel = 'CQPBZNR2S';
-const standUpChannel = 'CQN41THKN'
+const signing_secret = fs.readFileSync('./signing_secret.txt','utf-8');
+const slackEvents = createEventAdapter(signing_secret);
+const port = process.env.PORT || 3000;
 const rtm = new RTMClient(token);
 const messagetemplate = new messageTemplate();
+
+const testChannel = 'D013SPP9MFC';
+const chatChannel = 'CQPBZNR2S';
+const standUpChannel = 'CQN41THKN';
 
 let userschecker = new UsersChecker([]);
 let setUsersCheckerRule = makeScheduleRule({hour : 23, minute : 30, tz : 'Asia/Seoul', dayOfWeek :  new Schedule.Range(1,5)});
 let messageAbsentUsersRule = makeScheduleRule({hour : 1, minute : 30, tz : 'Asia/Seoul', dayOfWeek :  new Schedule.Range(1,5)});
-
 
 function messageTemplateAbsentUsers(){
     let template = messagetemplate.createAbsentMessageTemplate();
@@ -62,9 +66,7 @@ function makeScheduleRule(date){
     Schedule.scheduleJob(messageAbsentUsersRule, messageAbsentUsers);
 })(messageAbsentUsersRule, messageAbsentUsers);
 
-setUsersChecker();
-
-rtm.on('message', function(event){
+slackEvents.on('message', (event) => {
     if(event.channel == standUpChannel){
         userschecker.attend(event);
     }
@@ -74,30 +76,12 @@ rtm.on('message', function(event){
     if(event.text === "Who's absent"){
         rtm.postMessage(event.channel, messageTemplateAbsentUsers());
     }
-})
-
-rtm.start();
-*/
-const { createEventAdapter } = require('@slack/events-api');
-const slackEvents = createEventAdapter('116f1418fcd1f6798a2f2c8b37102be5');
-const port = process.env.PORT || 3000;
-const myRTM = require('./js/myRTM');
-const rtm = new myRTM('xoxb-1099856482231-1127785805591-jnernsHR4m9OyGIiaQ7gLyRi');
-
-// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
-slackEvents.on('message', (event) => {
-  if(event.text =='Hi'){
-    rtm.postMessage(event.channel,"Hi! how are you");
-  }
-  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
 });
 
-// Handle errors (see `errorCodes` export)
-slackEvents.on('error', console.error);
-rtm.postMessage("start");
-// Start a basic HTTP server
 slackEvents.start(port).then(() => {
-  // Listening on path '/slack/events' by default
   console.log(`server listening on port ${port}`);
 });
+
+setUsersChecker();
+
 
